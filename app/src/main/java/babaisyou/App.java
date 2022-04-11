d2fd16e0e5a251d3d51f18a12b82d6ca2346e59d
KAbaneko <100306755+KAbaneko@users.noreply.github.com>
2022-04-11 14:44:32 +0200

    Update App.java

--- app/src/main/java/babaisyou/App.java
+++ app/src/main/java/babaisyou/App.java
@@ -4,11 +4,11 @@
 package babaisyou;
 
 public class App {
-    public String getGreeting() {
+    public static String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(getGreeting());
     }
 }
